31ecedfefc04ca1778c605506b43f0226222ebaa
mdtoha21 <mdtohae@bu.edu>
2025-09-27 21:10:50 -0400

    Created Profile model for mini_insta

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Profile
+
 
 # Create your views here.
+
+
+class  ProfileListView(ListView):
+    '''Define a view class to show all mini_insta profiles'''
+    model=Profile
+    template_name="mini_insta/show_all_profiles.html"
+    context_object_name="profiles"
+    
+
+
