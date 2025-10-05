de8eaa6e34f7b68e1766916e04b3bb9180767763
mdtoha21 <mdtohae@bu.edu>
2025-10-05 15:58:14 -0400

    Added all files for deployment.

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Profile, Post, Photo
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
+from .forms import CreatePostForm
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -28,6 +30,37 @@ class PostDetailView(DetailView):
     template_name="mini_insta/show_post.html"
     context_object_name="post"
 
+
+class CreatePostView(CreateView):
+    model = Post
+    form_class = CreatePostForm
+    template_name = "mini_insta/create_post_form.html"
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        context['profile'] = profile
+        return context
+
+
+    def form_valid(self, form):
+        profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        post = form.save(commit=False)
+        post.profile = profile
+        post.save()
+
+ 
+        image_url = self.request.POST.get('image_url')
+        if image_url:
+            Photo.objects.create(post=post, image_url=image_url)
+        
+        self.object = post
+
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('show_post', args=[self.object.pk])
 
 
 
