081f8c7b0f612c3d5bc903c49111047280b63d3a
mdtoha <mdtoha@bu.edu>
2026-02-22 14:35:56 -0500

    Task 1

---
+++ mini_insta/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import *
+
+
+
+class  CreatePostForm(forms.ModelForm):
+
+    class Meta:
+        model=Post
+        fields = ['caption']
+
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView,CreateView
 from .models import Profile,Post,Photo
-
+from .forms import CreatePostForm
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
 class ProfileListView(ListView):
     '''Define a view class to show all mini_insta profiles'''
     model = Profile
@@ -19,3 +21,45 @@ class PostDetailView(DetailView):
     model=Post
     template_name="mini_insta/show_post.html"
     context_object_name="post"
+
+
+class CreatePostView(CreateView):
+    '''Allow users to create a new Post associated with a specific Profile.
+'''
+    model = Post
+    form_class = CreatePostForm
+    template_name = "mini_insta/create_post_form.html"
+
+
+    def get_context_data(self, **kwargs):
+
+        '''Add the Profile object to the context data
+        so the form knows which Profile this Post belongs to.'''
+
+        context = super().get_context_data(**kwargs)
+        profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        context['profile'] = profile
+        return context
+
+
+    def form_valid(self, form):
+        ''' Attaches the Post to the correct Profile and creates an associated Photo.'''
+        profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        post = form.save(commit=False)
+        post.profile = profile
+        post.save()
+
+ 
+        image_url = self.request.POST.get('image_url')
+        if image_url:
+            Photo.objects.create(post=post, image_url=image_url)
+        
+        self.object = post
+
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+         '''     Redirect the user to the new Post's detail page after creation.'''
+
+
+         return reverse('show_post', args=[self.object.pk])
